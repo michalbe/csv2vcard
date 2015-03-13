7397d7aec1108cb126212dbad2b7c1811b2b9257
Michał Budzyński <michal@virtualdesign.pl>
2015-03-13 01:01:00 +0100

    :neckbeard: get phone number form csv

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var file = 'contacts.csv';
 fs.readFile(file, 'utf8', function(err, output) {
   parse(output, function(err, parsedCSV){
     parsedCSV.forEach(function(contact){
-      console.log(contact[1], contact[3]);
+      console.log(contact[1], contact[3], contact[37]);
     });
   });
 });
