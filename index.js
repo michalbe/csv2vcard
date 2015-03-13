41b2127e8762ebd9ff5a30f4224e6fdcfd4e42f8
Michał Budzyński <michal@virtualdesign.pl>
2015-03-13 01:01:00 +0100

    :red_car: dislay only important data

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ var fs = require('fs');
 
 var file = 'contacts.csv';
 
-fs.readFile(file, 'utf8', function(err, output){
+fs.readFile(file, 'utf8', function(err, output) {
   parse(output, function(err, parsedCSV){
-    console.log(parsedCSV);
+    parsedCSV.forEach(function(contact){
+      console.log(contact[1], contact[3]);
+    });
   });
 });
