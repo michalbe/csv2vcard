8e4b6b44da7e738dd394366279264219d73f7c0d
Peter Dodds <peter@pddds.com>
2017-08-01 21:00:13 +0100

    Accept command line arguments (#24)

--- index.js
+++ index.js
@@ -2,26 +2,31 @@ var parse = require('csv-parse');
 var vCard = require('vcards-js');
 var fs = require('fs');
 
-var file = 'contacts.csv';
+var argv = require('minimist')(process.argv.slice(2), { alias: { o: 'output' }});
+
+var file = argv._[0] || 'contacts.csv';
 var vcardContact;
 var path;
+var outputDir = argv.output || './output';
 
-fs.readFile(file, 'utf8', function(err, output) {
-  parse(output, function(err, parsedCSV){
-    parsedCSV.forEach(function(contact) {
-      // positions from Outlook contacts schema in the README file of
-      // this project
-      if (contact[1] && contact[3] && contact[37]) {
-        vcardContact = vCard();
-        vcardContact.firstName = contact[1];
-        vcardContact.lastName = contact[3];
-        vcardContact.cellPhone = contact[37];
-        path = './output/' +
-          contact[1].toLowerCase().replace(/[\W]{1,}/ig, '-') + '-' +
-          contact[3].toLowerCase().replace(/[\W]{1,}/ig, '-') + '.vcf';
-        vcardContact.saveToFile(path);
-        console.log('File saved in ' + path);
-      }
+fs.mkdir(outputDir, function() {
+  fs.readFile(file, 'utf8', function(err, output) {
+    parse(output, function(err, parsedCSV){
+      parsedCSV.forEach(function(contact) {
+        // positions from Outlook contacts schema in the README file of
+        // this project
+        if (contact[1] && contact[3] && contact[37]) {
+          vcardContact = vCard();
+          vcardContact.firstName = contact[1];
+          vcardContact.lastName = contact[3];
+          vcardContact.cellPhone = contact[37];
+          path = outputDir + '/' +
+            contact[1].toLowerCase().replace(/[\W]{1,}/ig, '-') + '-' +
+            contact[3].toLowerCase().replace(/[\W]{1,}/ig, '-') + '.vcf';
+          vcardContact.saveToFile(path);
+          console.log('File saved in ' + path);
+        }
+      });
     });
   });
 });
