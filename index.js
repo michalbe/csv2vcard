9402b9f3b5541e95042f516b6519615a75f41628
Peter Dodds <peter@pddds.com>
2017-08-01 21:35:33 +0100

    get all phone numbers from csv

--- index.js
+++ index.js
@@ -9,41 +9,54 @@ var vcardContact;
 var path;
 var outputDir = argv.output || './output';
 
+function getColumns(header, match) {
+  var cols = [];
+  header.forEach(function(v, i) {
+    if (v.toLowerCase().match(match)) {
+      cols.push(i);
+    }
+  });
+  return cols;
+}
+
+function getNumbers(entry, cols) {
+  var r = cols.map(i => entry[i]).filter(v => (v !== undefined && v !== ""));
+  return r.length <= 1 ? r[0] : r;
+}
+
 fs.mkdir(outputDir, function() {
   fs.readFile(file, 'utf8', function(err, output) {
     parse(output, function(err, parsedCSV){
       var header = parsedCSV.shift();
       var firstNameCol = header.findIndex(v => v.toLowerCase() === 'first name');
+      var middleNameCol = header.findIndex(v => v.toLowerCase() === 'middle name');
       var lastNameCol = header.findIndex(v => v.toLowerCase() === 'last name');
-      var phoneColumns = [];
-      header.forEach(function(v, i) {
-        if (v.toLowerCase().match("phone")) {
-          phoneColumns.push(i);
-        }
-      });
-          console.log(firstNameCol, lastNameCol);
+      var cellPhoneCols = getColumns(header, /(primary|mobile) phone/);
+      var homePhoneCols = getColumns(header, 'home phone');
+      var workPhoneCols = getColumns(header, /(company.*|business|work|assistant.s) phone/);
+      var otherPhoneCols = getColumns(header, 'other phone');
 
       parsedCSV.forEach(function(contact) {
         // positions from Outlook contacts schema in the README file of
         // this project
         var firstName = contact[firstNameCol];
+        var middleName = contact[middleNameCol];
         var lastName = contact[lastNameCol];
 
-        var cellPhone;
-        for (var col of phoneColumns) {
-          if (contact[col]) {
-            cellPhone = contact[col];
-          }
-        }
-
-        if ((firstName || lastName) && cellPhone) {
+        if (firstName || lastName) {
           vcardContact = vCard();
           vcardContact.firstName = firstName;
+          vcardContact.middleName = middleName;
           vcardContact.lastName = lastName;
-          vcardContact.cellPhone = cellPhone
+
+          vcardContact.cellPhone = getNumbers(contact, cellPhoneCols);
+          vcardContact.homePhoneCols = getNumbers(contact, homePhoneCols);
+          vcardContact.workPhone = getNumbers(contact, workPhoneCols);
+          vcardContact.otherPhone = getNumbers(contact, otherPhoneCols);
+
           path = outputDir + '/';
           // join with space, trim extra space, replace whitespace with '-'
-          path += [firstName, lastName].join(' ').trim().toLowerCase().replace(/[\W]{1,}/ig, '-');
+          path += [firstName, middleName, lastName].join(' ').trim().toLowerCase().replace(/[\W]{1,}/ig, '-');
           path += '.vcf';
           vcardContact.saveToFile(path);
           console.log('File saved in ' + path);
