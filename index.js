a11716021baa3ccd2ed639c2c143b744d488a6d7
Peter Dodds <peter@pddds.com>
2017-08-01 20:31:16 +0100

    fix var name

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ var argv = require('minimist')(process.argv.slice(2), { alias: { o: 'output' }})
 var file = argv._[0] || 'contacts.csv';
 var vcardContact;
 var path;
-var output = argv.output || './output';
+var outputDir = argv.output || './output';
 
-fs.mkdir(output, function() {
+fs.mkdir(outputDir, function() {
   fs.readFile(file, 'utf8', function(err, output) {
     parse(output, function(err, parsedCSV){
       parsedCSV.forEach(function(contact) {
@@ -20,7 +20,7 @@ fs.mkdir(output, function() {
           vcardContact.firstName = contact[1];
           vcardContact.lastName = contact[3];
           vcardContact.cellPhone = contact[37];
-          path = output + '/' +
+          path = outputDir + '/' +
             contact[1].toLowerCase().replace(/[\W]{1,}/ig, '-') + '-' +
             contact[3].toLowerCase().replace(/[\W]{1,}/ig, '-') + '.vcf';
           vcardContact.saveToFile(path);
