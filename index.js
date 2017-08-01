fafcc89065802882d862ff37d145cf65abb879c8
Peter Dodds <peter@pddds.com>
2017-08-01 21:13:28 +0100

    Get the columns from the header, handle missing names

--- index.js
+++ index.js
@@ -12,17 +12,39 @@ var outputDir = argv.output || './output';
 fs.mkdir(outputDir, function() {
   fs.readFile(file, 'utf8', function(err, output) {
     parse(output, function(err, parsedCSV){
+      var header = parsedCSV.shift();
+      var firstNameCol = header.findIndex(v => v.toLowerCase() === 'first name');
+      var lastNameCol = header.findIndex(v => v.toLowerCase() === 'last name');
+      var phoneColumns = [];
+      header.forEach(function(v, i) {
+        if (v.toLowerCase().match("phone")) {
+          phoneColumns.push(i);
+        }
+      });
+          console.log(firstNameCol, lastNameCol);
+
       parsedCSV.forEach(function(contact) {
         // positions from Outlook contacts schema in the README file of
         // this project
-        if (contact[1] && contact[3] && contact[37]) {
+        var firstName = contact[firstNameCol];
+        var lastName = contact[lastNameCol];
+
+        var cellPhone;
+        for (var col of phoneColumns) {
+          if (contact[col]) {
+            cellPhone = contact[col];
+          }
+        }
+
+        if ((firstName || lastName) && cellPhone) {
           vcardContact = vCard();
-          vcardContact.firstName = contact[1];
-          vcardContact.lastName = contact[3];
-          vcardContact.cellPhone = contact[37];
-          path = outputDir + '/' +
-            contact[1].toLowerCase().replace(/[\W]{1,}/ig, '-') + '-' +
-            contact[3].toLowerCase().replace(/[\W]{1,}/ig, '-') + '.vcf';
+          vcardContact.firstName = firstName;
+          vcardContact.lastName = lastName;
+          vcardContact.cellPhone = cellPhone
+          path = outputDir + '/';
+          // join with space, trim extra space, replace whitespace with '-'
+          path += [firstName, lastName].join(' ').trim().toLowerCase().replace(/[\W]{1,}/ig, '-');
+          path += '.vcf';
           vcardContact.saveToFile(path);
           console.log('File saved in ' + path);
         }
